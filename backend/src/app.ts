458831325710eb227e6f78721885aa4de03e3831
Suryansh Ayush <22bds057@iiitdwd.ac.in>
2025-06-30 03:39:09 +0530

    updated app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,12 +10,14 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true}));
+app.use(cors({origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true, exposedHeaders: ["set-cookie"]}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //remove it in production
-app.use(morgan("dev"));
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 
 app.use("/api/v1", appRouter)
 
