15cffcd177bc61e9e2212b8f162de68065556999
Suryansh Ayush <22bds057@iiitdwd.ac.in>
2025-07-01 00:55:46 +0530

    updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,26 +9,11 @@ import cors from "cors";
 config();
 const app = express();
 //middlewares
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://mern-ai-tn68.vercel.app"
-];
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
-  exposedHeaders: ["set-cookie"]
+  exposedHeaders: ["set-cookie"],
 }));
-// app.use(cors({
-//   origin: process.env.FRONTEND_URL || "http://localhost:5173",
-//   credentials: true,
-//   exposedHeaders: ["set-cookie"],
-// }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
