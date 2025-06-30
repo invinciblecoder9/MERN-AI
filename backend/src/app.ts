c1de2f279279924f19a0dc38fc8c6cfdfa9dc8d9
Suryansh Ayush <22bds057@iiitdwd.ac.in>
2025-07-01 01:04:35 +0530

    Updated app.ts cors origin url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,11 +9,12 @@ import cors from "cors";
 config();
 const app = express();
 //middlewares
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true,
-  exposedHeaders: ["set-cookie"],
-}));
+app.use(cors({origin:"https://mern-ai-tn68.vercel.app", credentials: true}));
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//   credentials: true,
+//   exposedHeaders: ["set-cookie"],
+// }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
